9e3b6cee34bcdac337aebe57ce1614a49d6fa07b
kellbot <kellbot@gmail.com>
2023-06-18 18:02:25 -0400

    Added better data

--- SushiWeb/src/components/ItemPicker.vue
+++ SushiWeb/src/components/ItemPicker.vue
@@ -4,23 +4,18 @@
 
 			<v-card>
 				<v-tabs v-model="tab" bg-color="primary">
-					<v-tab value="one">Item One</v-tab>
-					<v-tab value="two">Item Two</v-tab>
-					<v-tab value="three">Item Three</v-tab>
+					<v-tab v-for="(category, index) in categories" :value="category.id">{{ category.name }}</v-tab>
+
 				</v-tabs>
 
 				<v-card-text>
 					<v-window v-model="tab">
-						<v-window-item value="one">
-							<ItemButton />
-						</v-window-item>
-
-						<v-window-item value="two">
-							Two
-						</v-window-item>
-
-						<v-window-item value="three">
-							Three
+						<v-window-item v-for="(category, index) in categories" :value="category.id">  
+							<v-container>
+							<v-row>
+								<ItemButton v-for="(recipe, index) in getRecipesByProducer('assembling-machine-2').filter(obj => {return obj.category === category.id})" :item="recipe" :icon="findIcon(recipe)" :index="index" />
+							</v-row>
+							</v-container>
 						</v-window-item>
 					</v-window>
 				</v-card-text>
@@ -34,12 +29,28 @@
 
 <script>
 import ItemButton from '@/components/itemButton.vue';
+import itemData from '@/assets/data.json';
+
 export default {
     data: () => ({
       tab: null,
+			items: itemData.items,
+			icons: itemData.icons,
+			categories: itemData.categories,
+			recipes: itemData.recipes,
+			 
+			findIcon: (item) => itemData.icons.filter(obj => {
+				return obj.id === item.id;
+			})[0],
+			getRecipesByProducer: (producer) => itemData.recipes.filter(obj => {
+				return obj.producers.includes(producer);
+			})
     }),  
+		name: 'ItemPicker',
 		components: {
     ItemButton
-		}
-  }
-</script>
+		},
+
+}
+
+</script>      
