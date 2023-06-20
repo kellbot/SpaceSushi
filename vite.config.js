f9bc7803880fdf24f6d59dff76a6c7a4d005c4e6
kellbot <kellbot@gmail.com>
2023-06-20 16:35:08 -0400

    Path fix

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/SpaceSushi/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
